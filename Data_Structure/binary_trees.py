11ee173871655d438b7149c77e990ed3434e0faa
Admin <sergius.la@gmail.com>
2019-05-19 17:28:10 -0700

    binaryTree

--- Data_Structure/binary_trees.py
+++ Data_Structure/binary_trees.py
@@ -11,6 +11,10 @@ class BinaryTree(object):
     def print_tree(self, traversal_type):
         if traversal_type == "preorder":
             return self.preorder_print(tree.root, "")
+        elif traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
+        elif traversal_type == "postorder":
+            return self.postorder_print(tree.root, "")
 
     def preorder_print(self, start, traversal):
         """Root->Left->Rigth"""
@@ -19,6 +23,36 @@ class BinaryTree(object):
             traversal = self.preorder_print(start.left, traversal)
             traversal = self.preorder_print(start.rigth, traversal)
         return traversal
+    
+    def inorder_print(self, root, traversal):
+        """Left->Root->Rigth"""
+        if root:
+            traversal = self.inorder_print(root.left, traversal)
+            traversal += (str(root.value) + " - ")
+            traversal = self.inorder_print(root.rigth, traversal)
+        return traversal
+    
+    def postorder_print(self, root, traversal):
+        """Left->Rigth->Root"""
+        if root:
+            traversal = self.postorder_print(root.left, traversal)
+            traversal = self.postorder_print(root.rigth, traversal)
+            traversal += (str(root.value) + " - ")
+        return traversal
+
+
+# Preorder:  1 - 2 - 4 - 5 - 3 - 6 - 7 - 8
+# Inorder:   4 - 2 - 5 - 1 - 6 - 3 - 7 - 8
+# Postorder: 4 - 5 - 2 - 6 - 8 - 7 - 3 - 1
+#
+#                 1
+#               /   \    
+#              /     \
+#             2       3
+#            / \     / \
+#           4   5   6   7
+#                        \       
+#                         8
 
 tree = BinaryTree(1)
 tree.root.left = Node(2)
@@ -29,5 +63,6 @@ tree.root.rigth.left = Node(6)
 tree.root.rigth.rigth = Node(7)
 tree.root.rigth.rigth.rigth = Node(8)
 
-x = tree.print_tree("preorder")
-print(x)
+preorder = tree.print_tree("preorder")
+inorder = tree.print_tree("inorder")
+postorder = tree.print_tree("postorder")
